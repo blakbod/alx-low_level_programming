e341ce2f3e3c8502741881dd2ca201cfcce5e59d
divine <divine.omale@gmail.com>
2022-09-13 07:06:17 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,14 @@
  *Description: using the main function
  *this program prints "Programming is like building a multilingual puzzle
  *Return: 0
- */
+*/
 
 int main(void)
+
 {
-	puts("\"Programming is like buiding a multilingual puzzle");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
