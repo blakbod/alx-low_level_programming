fc4af762c68b5aff90436598a2f7e1b6d148b4e4
divine <divine.omale@gmail.com>
2022-09-20 19:28:27 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code for alx students.
+ * @n: declaration of n and paramters for the function reset_to_98
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
