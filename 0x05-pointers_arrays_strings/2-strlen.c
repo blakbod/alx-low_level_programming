dcdc43bc3f33828bf04a9ed4ffc0eb0fdf065e43
divine <divine.omale@gmail.com>
2022-09-20 19:58:47 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * _strlen - check the code for alx students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
