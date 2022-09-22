95c4ff17c9e34a389bb92e9f285a5217ee5cc1f3
divine <divine.omale@gmail.com>
2022-09-22 15:03:05 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,18 @@
 #include "stdio.h"
 
 /**
- *
  * print_alphabet - function to print abc
- *
  * Return: 0
- *
  */
 
 void print_alphabet(void)
 
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+		putchar(c);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
